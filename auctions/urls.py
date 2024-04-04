7e178d7ce88baafa8f97592f3c86ddab5201ee98
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-04-04 11:49:34 +0800

    Finished the vanilla CS50 commerce porject

---
+++ auctions/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create", views.create, name="create"),
+    path("watchlist" , views.watchlist , name="watchlist"),
+    path("remove_watchlist" , views.remove_watchlist, name="remove_watchlist"),
+    path("add_watchlist" , views.add_watchlist , name="add_watchlist"),
+    path("place_bid" , views.place_bid , name="place_bid"),
+    path("closed_list", views.closed_list, name="closed_list"),
+    path("unactive_item", views.unactive_item , name="unactive_item"),
+    path("comment" , views.comment , name="create_comment"),
+    path("add_category" , views.add_category, name="add_category"),
+    path("<str:item>/" , views.item , name="item"),
+]
+ 
